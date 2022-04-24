5ba7b9ce9cd60939336b3938d701d80ce3209226
Zsr <1262426565@qq.com>
2022-04-24 17:18:20 +0800

    add pthread optimization version

--- GaussDePyramid-pThread.h
+++ GaussDePyramid-pThread.h
@@ -4,16 +4,25 @@
 
 #ifndef SIFT_GUASS_NORMAL_GAUSSDEPYRAMID_PTHREAD_H
 #define SIFT_GUASS_NORMAL_GAUSSDEPYRAMID_PTHREAD_H
-#include <iostream>
+#define THREAD_COUNT 7
 //
 // Created by zsr on 2022/4/17.
 //
-const double sigma = 2.0;
-const double PI = 3.1414926;
+//const double sigma = 2.0;
+//const double PI = 3.1414926;
 #include <iostream>
 #include <math.h>
+#include <pthread.h>
+class GaussPyramid_p;
+struct ThreadParam_t{
+    GaussPyramid_p* self;
+    int theLayer;//记录层数
+};//用在高斯金字塔同层间相减的参数
+
+
 class GaussPyramid_p{
 public:
+    pthread_barrier_t barrier_Division;
     int** data; //记录图像灰度数据
     GaussPyramid_p();
     GaussPyramid_p(int** img, int len/*,int wid*/, int S);
@@ -22,6 +31,8 @@ public:
     void output();
     void GaussFilter(int theLayer);
     void GenerateDoG();
+    ~GaussPyramid_p();
+    static void* thread_Filter_Sub(void *param);
 protected:
     int length;
     //int width;  先尝试宽度相同的版本
@@ -132,20 +143,84 @@ void GaussPyramid_p::GaussFilter(int theLayer) {//采用双边滤波
 }
 
 void GaussPyramid_p::GenerateDoG() {
-    int len=length;
+    pthread_barrier_init(&barrier_Division,NULL,THREAD_COUNT);
+    pthread_t handles[THREAD_COUNT];
+    ThreadParam_t paras[THREAD_COUNT];
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        paras[i]={this,i};
+        pthread_create(handles+i,NULL, thread_Filter_Sub,paras+i);
+    }
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        pthread_join(handles[i], nullptr);
+    }
+    pthread_barrier_destroy(&barrier_Division);
+}
+
+GaussPyramid_p::~GaussPyramid_p() {
+    int step=1;
     for (int i = 0; i < layer; ++i) {
-        GaussFilter(i);
-        for (int j = 0; j < S + 2; ++j) {
-            for (int k = 0; k < len; ++k) {
-                for (int l = 0; l < len; ++l) {
-                    GaussPy[i][j][k][l]-=GaussPy[i][j+1][k][l];
+        for (int j = 0; j < S + 3; ++j) {
+            for (int k = 0; k < length/step; ++k) {
+                delete []GaussPy[i][j][k];
+            }
+        }
+        step*=2;
+    }
+    for (int i = 0; i < layer; ++i) {
+        for (int j = 0; j < S + 3; ++j) {
+            delete []GaussPy[i][j];
+        }
+    }
+    for (int i = 0; i < layer; ++i) {
+        delete []GaussPy[i];
+    }
+    delete []GaussPy;
+}
+
+void *GaussPyramid_p::thread_Filter_Sub(void *param) {
+    ThreadParam_t * p=(ThreadParam_t *) param;
+    GaussPyramid_p* self=p->self;
+    int layer=self->layer;
+    int length=self->length;
+    double**** GaussPy=self->GaussPy;
+    int S=self->S;
+    int MyLayer=p->theLayer;
+    for (int i = MyLayer; i < layer; i+=THREAD_COUNT) {
+        int len=length;
+        int k=i;
+        while (k--){
+            len/=2;
+        }
+        double* fil=new double [len];
+        double l=double (len-1)/2.0;
+        for (int i = 0; i < S + 3; ++i) {
+            double sig=sigma/(i+1);
+            for (int i = 0; i < len; ++i) {
+                fil[i] = exp(-(i-l)*(i-l)/(2*sig*sig))/(sigma*sqrt(2*PI));
+            }
+            for (int j = 0; j < len; ++j) {
+                for (int k = 0; k < len; ++k) {
+                    GaussPy[MyLayer][i][j][k]*=fil[k];
+                }
+            }
+            for (int j = 0; j < len; ++j) {
+                for (int k = 0; k < len; ++k) {
+                    GaussPy[MyLayer][i][k][j]*=fil[k];
                 }
             }
         }
-        len/=2;
+        for (int i = 0; i < S + 2; ++i) {
+            for (int j = 0; j < len; ++j) {
+                for (int l = 0; l < len; ++l) {
+                    GaussPy[MyLayer][i][j][l]-=GaussPy[MyLayer][i+1][j][l];
+                }
+            }
+        }
+        delete []fil;
     }
+    pthread_barrier_wait(&self->barrier_Division);
+    pthread_exit(NULL);
 }
-
 
 
 #endif //SIFT_GUASS_NORMAL_GAUSSDEPYRAMID_PTHREAD_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "GuassDePyramid.h"
+#include "GaussDePyramid-pThread.h"
 #include <sys/time.h>
 using namespace std;
 const int MAX=4096;
-int n=128;
+int n=8;
 timeval start, final;
 int main() {
     int** p=new int* [MAX];
@@ -12,21 +13,23 @@ int main() {
     }
     for (int i = 0; i < MAX; ++i) {
         for (int j = 0; j < MAX; ++j) {
-            p[i][j]=i+j;
+            p[i][j]=1;
         }
     }
-
-    for (int n = 32; n <= MAX; n*=2) {
-        int count=0;
-        gettimeofday(&start,NULL);
-        gettimeofday(&final,NULL);
-        while (final.tv_sec-start.tv_sec<5.0){
-            count++;
-            GaussPyramid g(p, n, 2);
-            g.GenerateDoG();
-            gettimeofday(&final,NULL);
-        }
-        cout<<n<<","<<(final.tv_sec-start.tv_sec)/(1000.0*count)<<endl;
-    }
+    GaussPyramid gaussPyramidP(p,n,2);
+    gaussPyramidP.GenerateDoG();
+    gaussPyramidP.output();
+//    for (int n = 32; n <= MAX; n*=2) {
+//        int count=0;
+//        gettimeofday(&start,NULL);
+//        gettimeofday(&final,NULL);
+//        while (final.tv_sec-start.tv_sec<5.0){
+//            count++;
+//            GaussPyramid g(p, n, 2);
+//            g.GenerateDoG();
+//            gettimeofday(&final,NULL);
+//        }
+//        cout<<n<<","<<(final.tv_sec-start.tv_sec)/(count)<<endl;
+//    }
     return 0;
 }
