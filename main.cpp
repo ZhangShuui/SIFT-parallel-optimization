cc32351fd72d3825908c3a1aa44bb18cb7b149ca
Zsr <1262426565@qq.com>
2022-05-19 20:54:08 +0800

    add openMPxAVX and openMPxAVX512 edition and debug them

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "GuassDePyramid.h"
 //#include "GaussDePyramid-NEON.h"
+#include "GaussDePyramid-AVXxOpenMP.h"
 #include "GaussDePyramid-OpenMP.h"
-#include "GaussDePyramid-NEONxOpenMP.h"
+#include "GaussDePyramid-AVX512xOpenMP.h"
+//#include "GaussDePyramid-NEONxOpenMP.h"
 #include <vector>
 //#include "GaussDePyramid-SSExPTHREAD.h"
 //#include "GaussDePyramid-AVX512xPTHREAD.h"
@@ -27,14 +29,19 @@ int main() {
     }
     float total = 0.0;
     int count = 0;
+    float total2 =0.0;
+    int count2 =0;
+    float total3 =0.0;
+    int count3 =0;
     n = 8;
     int k = 1;
-    while (n<=8192){
-        GaussPyramid_nomp g_omp(p, n, 2);
+    while (n<=4096){
+        GaussPyramid_aomp g_omp(p, n, 2);
         GaussPyramid_omp g(p,n,2);
+        GaussPyramid_a512omp ga(p,n,2);
         total =0.0;
         count =0;
-        while (total< 10.0){
+        while (total< 6.0){
             g_omp.GaussPyInit();
             gettimeofday(&start, NULL);
             g_omp.GenerateDoG_nomp_dynamic();
@@ -43,9 +50,9 @@ int main() {
             count++;
         }
 
-        float total2 =0.0;
-        int count2 =0;
-        while (total2< 10.0){
+        total2 =0.0;
+        count2 =0;
+        while (total2< 6.0){
             g.GaussPyInit();
             gettimeofday(&start, NULL);
             g.GenerateDoG_omp();
@@ -53,11 +60,22 @@ int main() {
             total2 += final.tv_sec - start.tv_sec;
             count2++;
         }
-        std::cout <<n<<","  << total / float(count) <<","<<total2 / float(count2)<<endl;
+
+        total3 =0.0;
+        count3 =0;
+        while (total3< 6.0){
+            ga.GaussPyInit();
+            gettimeofday(&start, NULL);
+            ga.GenerateDoG_nomp_dynamic();
+            gettimeofday(&final, NULL);
+            total3 += final.tv_sec - start.tv_sec;
+            count3++;
+        }
+        std::cout <<n<<","<<total2 / float(count2)<<","  << total / float(count) <<","<<total3 / float(count3)<<endl;
         n*=2;
 //        k++;
     }
-//    GaussPyramid_omp g(p,8,2);
+//    GaussPyramid_omp g(p,16,2);
 //    g.GaussPyInit();
 //    g.GenerateDoG_omp();
 //    g.output();
@@ -65,7 +83,11 @@ int main() {
 //    gg.GaussPyInit();
 //    gg.GenerateDoG();
 //    gg.output();
-//    GaussPyramid_nomp ggg(p,8,2);
+//    GaussPyramid_aomp ggg(p,8,2);
+//    ggg.GaussPyInit();
+//    ggg.GenerateDoG_nomp_dynamic();
+//    ggg.output();
+//    GaussPyramid_a512omp ggg(p,16,2);
 //    ggg.GaussPyInit();
 //    ggg.GenerateDoG_nomp_dynamic();
 //    ggg.output();
